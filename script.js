81f79d3991d8effc5f2878960883f58b7a8f2bd9
gabrusevs <gabrusevs@gmail.com>
2018-08-14 14:28:02 +0300

    Add onload run

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var title = document.getElementsByTagName("title")[0];
-title.innerHTML = "External js title";
-var content = document.getElementById("content");
-content.innerHTML = "External js content";
+window.onload = function() {
+  var title = document.getElementsByTagName("title")[0];
+  title.innerHTML = "External js title";
+  var content = document.getElementById("content");
+  content.innerHTML = "External js content";
 
-var xhr = new XMLHttpRequest()
-xhr.open('GET', 'data.json', true)
-xhr.onload = function (e) {
-  xhr = JSON.parse(xhr.response)
-  title.innerHTML = xhr.title
-  content.innerHTML = xhr.content
-}
-xhr.send(null)
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "data.json", true);
+  xhr.onload = function(e) {
+    xhr = JSON.parse(xhr.response);
+    title.innerHTML = xhr.title;
+    content.innerHTML = xhr.content;
+  };
+  xhr.send(null);
+};
